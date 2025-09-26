8ca1fcb0c2a80f8b66f32c8036909f1c3b3317b4
Nghia <0306231311@caothang.edu.vn>
2025-09-26 16:43:27 +0700

    fix xog thanh tim kiem

---
+++ BT2_Nhom3_23WebC/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using BT2_Nhom3_23WebC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT2_Nhom3_23WebC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var products = new List<Product>
+            {
+            new Product { Name = "Ut eu feugiat", ImageUrl = "/images/product/01.jpg", Alt = "floral set 1", Price = 240 },
+            new Product { Name = "Donec Est Nisi", ImageUrl = "/images/product/02.jpg", Alt = "flowers 2", Price = 160 },
+
+            
+            new Product { Name = "Tristique Vitae", ImageUrl = "/images/product/03.jpg", Alt = "flowers 3", Price = 180 },
+             new Product {Name = "Hendrerit Eu", ImageUrl="/images/product/04.jpg", Alt = "flowers 3",Price= 320 },
+
+
+            new Product { Name = "Hendrerit Eu", ImageUrl = "/images/product/04.jpg", Alt = "flowers 4", Price = 320, NoMarginRight = true },
+       
+            };
+            return View(products);
+            
+        }
+    }
+}
---
+++ BT2_Nhom3_23WebC/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BT2_Nhom3_23WebC.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Alt { get; set; }
+        public decimal Price { get; set; }
+        public bool NoMarginRight { get; set; }
+    }
+}
