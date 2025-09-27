850c6de97223d293158d72a8422ce41bada5bfa3
Nghia <0306231311@caothang.edu.vn>
2025-09-28 02:38:52 +0700

    areas phan quan ly cua admin

---
+++ BT2_Nhom3_23WebC/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BT2_Nhom3_23WebC.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Admin Product Page";
+            return View();
+        }
+    }
+}
--- BT2_Nhom3_23WebC/Program.cs
+++ BT2_Nhom3_23WebC/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -25,10 +25,19 @@ app.MapStaticAssets();
 
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+app.UseEndpoints(endpoints =>
+{
+    // Routing cho Area (Admin)
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    // Routing mặc định cho người dùng
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
 
 
 app.Run();
